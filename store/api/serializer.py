d84b4b9e5d6a690285c108e681e720dae88db2e0
SaebyeokShin <toqur1219@naver.com>
2023-01-09 10:34:52 +0000

    feat:serializing realationshinps

--- store/api/serializer.py
+++ store/api/serializer.py
@@ -3,12 +3,19 @@ from rest_framework import serializers
 from store.models import Category, Product
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = [ "id", "name", "description", "category", "slug", "inventory", "old_price", "price"]
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["category_id", "title", "slug"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [ "id", "name", 'image', "description", "category", "slug", "inventory", "old_price", "price", "top_deal", "flash_sales"]
+
+    category = CategorySerializer()
+    #category = serializers.StringRelatedField()
+
+
